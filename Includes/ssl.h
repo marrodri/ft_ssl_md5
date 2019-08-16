bdb41315ac3a1402926ba172906b07fa7de31dcc
marrodri <laza.mar64@gmail.com>
2019-08-16 11:28:27 -0700

    md5 start

---
+++ Includes/ssl.h
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ssl.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marrodri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/12 10:30:48 by marrodri          #+#    #+#             */
+/*   Updated: 2019/08/12 10:30:50 by marrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SSL_H
+# define SSL_H
+# include "Includes/libft/libft.h"
+# include "Includes/ft_printf/ft_printf.h"
+# include <fcntl.h>
+# define CI_COM {'p','q','r','s'}
+# define HS_COM {"md5", "sha256"}
+# define HS_SZ 2
+# define CI_SZ 4
+# define A0 0x67452301
+# define B0	0xefcdab89
+# define C0 0x98badcfe
+# define D0 0x10325476
+# define F(B,C,D) ((B & C) | (~B & D))
+# define G(B,C,D) ((B & D) | (C & ~D))
+# define H(B,C,D) (B ^ C ^ D)
+# define I(B,C,D) (C ^ (B | ~D))
+# define R_LEFT(F,S) ((F << S) | (S >> (32 - S)))
+
+
+typedef struct		s_flag
+{
+	unsigned int	p;
+	unsigned int	q;
+	unsigned int	r;
+	unsigned int	s;
+}					t_flag;
+
+typedef struct		s_md5v
+{
+	unsigned int	A;
+	unsigned int	B;
+	unsigned int	C;
+	unsigned int	D;
+}					t_md5v;
+
+// typedef		*hash_funct();
+
+typedef unsigned int t_uint;
+void	md5_hash(char *mssg, t_flag *flags);
+
+#endif
+
+//TODO files to keep for printf
+//ft_llitoa_base;
+//ft_ullitoa_base;
+//ft_strupper;
+//ft_strsub;
+//ft_strjoin;
+//ft_swap_charstr;
+//ft_strnewc;
+//ft_strlen;
+//ft_atoi;
+//ft_ldtoa;
+
+//inside printf libft, files to keep
--- ssl_md5.c
+++ ssl_md5.c
@@ -3,20 +3,81 @@
 
 //  unsigned int[64] s; shifts
 //	unsigned int[64] K; KEYS
+
+// leftrotate bits function by 32 bits
+
 //check header for the exact numbers
+// # define K0 { 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee }
+// # define K1 { 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501 }
+// # define K2 { 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be }
+// # define K3 { 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821 }
 
-// leftrotate bits function
+// # define K4	{ 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa }
+// # define K5	{ 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8 }
+// # define K6	{ 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed }
+// # define K7	{ 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a }
 
-unsigned int rotate_left(unsigned int F, unsigned int s)
-{
-	return (F << s) | (s >> (32 - s));
-}
+// # define K8	{ 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c }
+// # define K9	{ 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70 }
+// # define K10	{ 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05 }
+// # define K11	{ 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665 }
+
+// # define K12 { 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039 }
+// # define K13 { 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1 }
+// # define K14 { 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1 }
+// # define K15 { 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391 }
+
+// s[ 0..15] := { 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22 }
+// s[16..31] := { 5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20 }
+// s[32..47] := { 4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23 }
+// s[48..63] := { 6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 }
 
 #include "ssl.h"
 
-unsigned int a0 = 0x67452301;
-unsigned int b0 = 0xefcdab89;
-unsigned int c0 = 0x98badcfe;
-unsigned int d0 = 0x10325476;
 
-//first add 1 bit to the message
+
+t_uint get_str_sum(char *str)
+{
+	t_uint val;
+
+	val = 0;
+	while(*str)
+	{
+		val += *str;
+		*str++;
+	}
+	return val;
+}
+
+t_uint	padded_mod(t_uint val)
+{
+	//first add 1 bit to the message padded
+	//if the added 1 bit doesn't return the exact val mod 512
+	//then padd the value with 0 bits until getting the value mod 512
+	return 0;
+}
+
+void	md5v_init(t_md5v **md5Vr)
+{
+	(*md5Vr)->A = A0;
+	(*md5Vr)->B = B0;
+	(*md5Vr)->C = C0;
+	(*md5Vr)->D = D0;
+}
+
+void	md5_hash(char *mssg, t_flag *flags)
+{
+	t_md5v *md5Vr;
+	t_uint	val;
+
+	md5Vr = malloc(sizeof(t_md5v));
+	md5v_init(&md5Vr);
+	val = get_str_sum(mssg);
+
+	//modify the value by padding
+
+	//apped original length in bits mod 2^64 to message TRY OR LEARN HOW TO
+
+	//proccess the message in successive 512-bit chunks 2 while loops
+}
+
