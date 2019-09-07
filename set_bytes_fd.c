bd2c823d23fdeecfaecd40372dbb0156f11eed42
marrodri <laza.mar64@gmail.com>
2019-09-07 01:10:28 -0700

    set_bytes_fd.c backup

--- set_bytes_fd.c
+++ set_bytes_fd.c
@@ -12,39 +12,64 @@
 
 #include "ssl.h"
 
+void	append_last_chunk(t_list **list, uint8_t *tmp, uint32_t bytes, uint32_t byte_len)
+{
+	uint64_t bit_len;
+
+	tmp = ft_memalloc(bytes);
+	ft_bzero(tmp, bytes);
+	tmp[0] = 0x80;
+	bit_len = byte_len * 8;
+	tmp = ft_append_bitlen(tmp, bytes, bit_len);
+	add_new_chunk(list, tmp, bytes);
+}
+
+void	append_new_chunk(t_list **list, uint8_t *tmp, uint32_t bytes, uint64_t bit_len)
+{
+	add_new_chunk(list, tmp, bytes);
+	tmp = ft_memalloc(bytes);
+	ft_bzero(tmp, bytes);
+	tmp = ft_append_bitlen(tmp, bytes, bit_len);
+	add_new_chunk(list, tmp, bytes);
+}
+
+void	set_mssg(t_hash **hash_v, char *tmp, char *buff)
+{
+	if ((*hash_v)->mssg)
+		(*hash_v)->mssg = ft_strjoin(tmp, buff);
+	else
+		(*hash_v)->mssg = ft_strdup(buff);
+}
 
 void	set_bytes_fd(const int fd, uint32_t bytes, t_list **list, t_hash **hash_v)
 {
-	uint32_t ret;
-	uint8_t buff[bytes];
-	uint32_t byte_len;
-	uint64_t bit_len;
-	uint8_t *tmp;
-	uint8_t *extra;
+	uint32_t	ret;
+	uint8_t		buff[bytes];
+	uint32_t	byte_len;
+	uint64_t	bit_len;
+	uint8_t		*tmp;
 
 	bit_len = 0;
 	byte_len = 0;
 	(*hash_v)->mssg = NULL;
-	while((ret = read(fd, buff, bytes)) > 0)
+	while ((ret = read(fd, buff, bytes)) > 0)
 	{
 		byte_len = byte_len + ret;
 		tmp = ft_memalloc(bytes);
 		tmp = ft_memcpy(tmp, buff, bytes);
-		if((*hash_v)->mssg)
-			(*hash_v)->mssg = ft_strjoin((char*)tmp, (char *)buff);
-		else
-			(*hash_v)->mssg = ft_strdup((char*)buff);
+		set_mssg(hash_v, (char*)tmp, (char*)buff);
 		if (ret < bytes)
 		{
 			bit_len = byte_len * 8;
 			tmp = ft_append_bytes(tmp, ret, bytes);
-			if(!check_last8bytes(tmp, bytes))
+			if (!check_last8bytes(tmp, bytes))
 			{
-				add_new_chunk(list, tmp, bytes);
-				extra = ft_memalloc(bytes);
-				ft_bzero(extra, bytes);
-				extra = ft_append_bitlen(extra, bytes, bit_len);
-				add_new_chunk(list, extra, bytes);
+				// add_new_chunk(list, tmp, bytes);
+				// tmp = ft_memalloc(bytes);
+				// ft_bzero(tmp, bytes);
+				// tmp = ft_append_bitlen(tmp, bytes, bit_len);
+				// add_new_chunk(list, tmp, bytes);
+				append_new_chunk(list, tmp, bytes, bit_len);
 				return ;
 			}
 			else
@@ -52,14 +77,15 @@ void	set_bytes_fd(const int fd, uint32_t bytes, t_list **list, t_hash **hash_v)
 		}
 		add_new_chunk(list, tmp, bytes);
 	}
-	if(byte_len % bytes == 0)
+	if (byte_len % bytes == 0)
 	{
-		tmp = ft_memalloc(bytes);
-		ft_bzero(tmp, bytes);
-		tmp[0] = 0x80;
-		bit_len = byte_len * 8;
-		tmp = ft_append_bitlen(tmp, bytes, bit_len);
-		add_new_chunk(list, tmp, bytes);
+		// tmp = ft_memalloc(bytes);
+		// ft_bzero(tmp, bytes);
+		// tmp[0] = 0x80;
+		// bit_len = byte_len * 8;
+		// tmp = ft_append_bitlen(tmp, bytes, bit_len);
+		// add_new_chunk(list, tmp, bytes);
+		append_last_chunk(list, tmp, bytes, byte_len);
 	}
 	return ;
 }
