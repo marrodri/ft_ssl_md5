5b47f1d41e4a4975e45a96b978659e46a4d035cf
marrodri <laza.mar64@gmail.com>
2019-09-07 03:42:11 -0700

    set_bytes_str backup, mostly comments for emergency

--- set_bytes_str.c
+++ set_bytes_str.c
@@ -38,70 +38,61 @@ void	set_buff_str(t_lstcon **lstv, uint32_t bytes, int i)
 	(*lstv)->tmp = ft_append_bytes((*lstv)->tmp, i, bytes);
 }
 
-void	set_new_chunk_str(t_lstcon **lstv, t_list **list, uint32_t bytes)
-{
-	(*lstv)->tmp = ft_memalloc(bytes);
-	(*lstv)->tmp = ft_memcpy((*lstv)->tmp, (*lstv)->buff, bytes);
-	ft_bzero((*lstv)->buff, bytes);
-	add_new_chunk(list, (*lstv)->tmp, bytes);
-}
-
-
-void	set_bytes_str(char *str, uint32_t bytes, t_list **list)
+int	set_new_chunk_str(t_lstcon **lstv, t_list **list, uint32_t bytes, char *str)
 {
 	int i;
-	t_lstcon *lstv;
-
-	// uint8_t *tmp;
-	// uint8_t chunk[bytes];
-	// uint32_t byte_len;
-	// uint64_t bit_len;
-	// byte_len = 0;
-
-	init_varlst(&lstv, bytes);
 	i = 0;
-	while (str[lstv->byte_len])
+	// (*lstv)->tmp = ft_memalloc(bytes);
+	// (*lstv)->tmp = ft_memcpy((*lstv)->tmp, (*lstv)->buff, bytes);
+	// ft_bzero((*lstv)->buff, bytes);
+	// add_new_chunk(list, (*lstv)->tmp, bytes);
+	while (str[(*lstv)->byte_len])
 	{
 		if (i % bytes == 0 && i != 0)
 		{
 			i = 0;
-			set_new_chunk_str(&lstv, list, bytes);
-			// lstv->tmp = ft_memalloc(bytes);
-			// lstv->tmp = ft_memcpy(lstv->tmp, lstv->buff, bytes);
-			// ft_bzero(lstv->buff, bytes);
-			// add_new_chunk(list, lstv->tmp, bytes);
+			(*lstv)->tmp = ft_memalloc(bytes);
+			(*lstv)->tmp = ft_memcpy((*lstv)->tmp, (*lstv)->buff, bytes);
+			ft_bzero((*lstv)->buff, bytes);
+			add_new_chunk(list, (*lstv)->tmp, bytes);
+			// set_new_chunk_str(&lstv, list, bytes, &i);
 		}
-		lstv->buff[i] = str[lstv->byte_len];
-		lstv->byte_len++;
+		(*lstv)->buff[i] = str[(*lstv)->byte_len];
+		(*lstv)->byte_len++;
 		i++;
 	}
+	return i;
+}
+
+void	set_bytes_str(char *str, uint32_t bytes, t_list **list)
+{
+	int			i;
+	t_lstcon	*lstv;
+
+	init_varlst(&lstv, bytes);
+	i = 0;
+	// while (str[lstv->byte_len])
+	// {
+	// 	if (i % bytes == 0 && i != 0)
+		// {
+	// 			*i = 0;
+	// (*lstv)->tmp = ft_memalloc(bytes);
+	// (*lstv)->tmp = ft_memcpy((*lstv)->tmp, (*lstv)->buff, bytes);
+	// ft_bzero((*lstv)->buff, bytes);
+	// add_new_chunk(list, (*lstv)->tmp, bytes);
+		i =	set_new_chunk_str(&lstv, list, bytes, str);
+		// }
+	// 	lstv->buff[i] = str[lstv->byte_len];
+	// 	lstv->byte_len++;
+	// 	i++;
+	// }
 	set_buff_str(&lstv, bytes, i);
-	// lstv->bit_len = lstv->byte_len * 8;
-	// lstv->tmp = ft_memalloc(bytes);
-	// lstv->tmp = ft_memcpy((char*)lstv->tmp, (char*)lstv->buff, bytes);
-	// lstv->tmp = ft_append_bytes(lstv->tmp, i, bytes);
 	if (i % bytes == 0)
-	{
 		append_1bit_chunk_str(&lstv, list, bytes);
-		// add_new_chunk(list, lstv->tmp, bytes);
-		// lstv->tmp = ft_memalloc(bytes);
-		// lstv->tmp = ft_append_bytes(lstv->tmp, 0, bytes);
-		// lstv->tmp = ft_append_bitlen(lstv->tmp, bytes, lstv->bit_len);
-		// add_new_chunk(list, lstv->tmp, bytes);
-	}
 	else
-	{	
-		if(!check_last8bytes(lstv->tmp, bytes))
-		{
+	{
+		if (!check_last8bytes(lstv->tmp, bytes))
 			append_last_chunk_str(&lstv, list, bytes);
-			// add_new_chunk(list, lstv->tmp, bytes);
-			// lstv->tmp = ft_memalloc(bytes);
-			// ft_bzero(lstv->tmp, bytes);
-			// lstv->tmp = ft_append_bitlen(lstv->tmp, bytes, lstv->bit_len);
-			// add_new_chunk(list, lstv->tmp, bytes);
-			free(lstv);
-			return ;
-		}
 		else
 		{
 			lstv->tmp = ft_append_bitlen(lstv->tmp, bytes, lstv->bit_len);
